bc4fb7c5386b04cec2acbc10b29cb6f5b430b8b4
Cristian David Home <cristian_david033@hotmail.com>
2024-06-26 23:06:42 -0500

    Add typescript.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome</h1>
+    <span>The home page</span>
   </main>
 </template>
